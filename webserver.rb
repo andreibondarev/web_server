e1ef613aa693483f5737b77c69a76b7f680f8d8e
Andrei Bondarev <andrei.bondarev13@gmail.com>
2013-02-05 22:49:05 -0500

    Initial version

---
+++ webserver.rb
@@ -0,0 +1,25 @@
+## BrightRoll 2/4/12 - Andrei Bondarev
+
+require 'socket'
+require 'logger'
+
+logger = Logger.new('log.txt')
+
+server = TCPServer.new('localhost', 3000)
+
+loop do
+	session = server.accept
+	session.print "HTTP/1.1 200 OK\n"
+	
+	resource = session.gets.split[1].gsub('/','')
+
+	if resource.empty?
+		request_line = session.gets while !(request_line =~ /^Cookie/)
+		session.print "\n#{request_line}"
+	else
+		session.print "Set-Cookie: segments=#{resource} Expires=Wed, 01-Jan-2020 12:12:12 GMT;\n\n"
+	end
+	
+	logger.info "#{session.gets}#{session.gets}"
+	session.close
+end

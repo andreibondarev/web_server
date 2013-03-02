b0cc54998a224caff9337ec449172ffaf53766a4
Andrei Bondarev <andrei.bondarev13@gmail.com>
2013-03-02 01:02:48 -0500

    Moving the whole thing into a wrapper class, delegating functionality to methods

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 <h2>Web Server Challenge:</h2>
 
-<h2>Milestones:</h2>
+<h3>Milestones:</h3>
 <ol>
 	<li>Respond with ‘Hello World’ and return 200 OK</li>
-	<li>Set a cookie when a user visit the server</li>
-	<li>Set a cookie with that requested ID</li>
-	<li>Go to root -> Print out the value of the cookie </li>
-	<li>Look up in CSV and display that string</li>
+	<li>Set a cookie when a user visits</li>
+	<li>Set a 'segments' cookie with the requested path</li>
+	<li>Print the value of 'segments' when a user visit root path</li>
+	<li>CSV look up the path and display the value</li>
 	<li>Append to existing cookie values</li>
 </ol>
--- webserver.rb
+++ webserver.rb
@@ -1,23 +1,52 @@
 require 'socket'
 require 'logger'
 
-logger = Logger.new('log.txt')
-
-server = TCPServer.new('localhost', 3000)
-
-loop do
-	session = server.accept
-	session.print "HTTP/1.1 200 OK\n"
-	
-	resource = session.gets.split[1].gsub('/','')
-
-	if resource.empty?
-		request_line = session.gets while !(request_line =~ /^Cookie/)
-		session.print "\n#{request_line}"
-	else
-		session.print "Set-Cookie: segments=#{resource} Expires=Wed, 01-Jan-2020 12:12:12 GMT;\n\n"
+class WebServerWrapper
+	def initialize
+		@server = TCPServer.new('localhost', 3000)
+		@logger = Logger.new('log.txt')
 	end
-	
-	logger.info "#{session.gets}#{session.gets}"
-	session.close
+
+	def start!
+		loop do
+			session = @server.accept
+			session.print "HTTP/1.1 200 OK\n"
+			
+			id = get_id(session)
+
+			cookies_line = ""
+			while((line = session.gets) != "\r\n")
+				cookies_line = line if line =~ /^Cookie/
+				print line
+			end
+
+			if id.empty?
+				session.print "\n#{cookies_line}" if !cookies_line.empty?
+			else
+				segments_value = find_segments_value(cookies_line) if !cookies_line.empty?
+				
+				session.print "Set-Cookie: segments=#{segments_value},#{id} Expires=Wed, 01-Jan-2020 12:12:12 GMT;\n\n" #FIXME
+			end
+			
+			session.close
+		end		
+	end
+
+	def close_session(session)
+		session.close
+	end
+
+	def get_id(session)
+		session.gets.split[1].gsub('/','')
+	end
+
+	def find_segments_value(request_line)
+		segments_str = request_line.scan(/\ssegments=[a-zA-z1-9,]*\s{1}/).first
+		segments_str.gsub('segments=','')
+	end
 end
+
+ws = WebServerWrapper.new
+ws.start!
+
+
